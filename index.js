a2371951acfa3f0c072277e0d4bd0625592d20f7
ascvhbjnk <gs1haritada@hotmail.com>
2024-05-05 08:18:34 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
