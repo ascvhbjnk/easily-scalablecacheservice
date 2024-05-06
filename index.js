5fa5e9506549557012dc0b062bffd5fac105faf6
ascvhbjnk <gs1haritada@hotmail.com>
2024-05-06 23:58:39 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function reverseList(head) {
+function isValidBST(root) {
   let prev = null;
-  let curr = head;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
+  return inorder(root);
+  function inorder(node) {
+    if (!node) return true;
+    if (!inorder(node.left)) return false;
+    if (prev !== null && node.val <= prev) return false;
+    prev = node.val;
+    return inorder(node.right);
   }
-  return prev;
 }
