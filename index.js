39d1e17ed7efcfc3469a8544ad944b20bb784bd7
ascvhbjnk <gs1haritada@hotmail.com>
2024-05-11 07:19:20 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-function minDepth(root) {
-  if (!root) return 0;
-  if (!root.left && !root.right) return 1;
-  let min = Infinity;
-  if (root.left) min = Math.min(min, minDepth(root.left));
-  if (root.right) min = Math.min(min, minDepth(root.right));
-  return min + 1;
-}
+const countOccurrences = (arr, value) =>
+  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
